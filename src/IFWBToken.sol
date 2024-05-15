6dd0fb88c563ea35749fad3656f9843b52ecf9ef
ysfkel <yusufkel@gmail.com>
2024-05-15 22:32:42 +0400

    added migrations manager

---
+++ src/IFWBToken.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {IERC20} from "@openzeppelin/token/ERC20/IERC20.sol";
+
+interface IFWBToken is IERC20 {
+
+    function burn(uint256 value) external;
+}
---
+++ src/MigrationManager.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+ 
+import "./IFWBToken.sol";
+import "@openzeppelin/access/AccessControl.sol";
+
+contract MigrationMinager is AccessControl{
+
+    event Deposit(address account, address recipient, uint256 amount);
+
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    address public owner;
+    mapping(address => uint256) public deposits;
+    mapping(address from => mapping(address to =>  uint256 amount)) public depositsTo;
+    IFWBToken public migrationToken;
+
+    constructor(address _migrationToken,  address burner) {
+        require(_migrationToken != address(0), "0x0 token");
+        require(burner != address(0), "0x0 token");
+       
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(BURNER_ROLE, burner);
+        migrationToken = IFWBToken(_migrationToken);
+    }
+
+    function deposit(uint256 amount, address ) external {
+       _deposit(amount, msg.sender);
+    }
+
+    function depositTo(uint256 amount, address to) external {
+       _deposit(amount, to);
+    }
+
+    function burn(uint256 amount) public onlyRole(BURNER_ROLE) {
+        migrationToken.burn(amount);
+    } 
+
+    function _deposit(uint256 amount, address to ) private {
+       deposits[msg.sender] += amount;
+       depositsTo[msg.sender][msg.sender] += amount;
+       migrationToken.transferFrom(msg.sender, address(this), amount);
+       emit Deposit(msg.sender, to, amount);
+    }
+
+}
