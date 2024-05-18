e92786e65e304f2f047c4f5885f60e9d70ee43f2
Edward Kim <edk217@gmail.com>
2024-05-18 15:55:24 -0400

    migration manager with deposit struct and lookup

--- src/MigrationManager.sol
+++ src/MigrationManager.sol
@@ -1,43 +1,63 @@
-// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
- 
-import "./IFWBToken.sol";
-import "@openzeppelin/access/Ownable.sol";
 
-contract MigrationMinager is Ownable{
+import "@openzeppelin/token/ERC20/IERC20.sol";
 
-    event Deposit(address account, address recipient, uint256 amount);
+contract MigrationManager {
+    IERC20 public fwbToken;
+    uint256 public depositCount;
 
-    mapping(address => uint256) public deposits;
-    mapping(address from => mapping(address to =>  uint256 amount)) public depositsTo;
-    IFWBToken public migrationToken;
-
-    constructor(address _migrationToken) Ownable(msg.sender) {
-        require(_migrationToken != address(0), "0x0__migrationToken");
-        migrationToken = IFWBToken(_migrationToken);
+    struct DepositInfo {
+        address depositor;
+        address recipient;
+        uint256 amount;
     }
-     
+
+    DepositInfo[] public deposits;
+
+    event Deposit(address indexed depositor, address indexed recipient, uint256 amount, uint256 depositId);
+
+    constructor(address _fwbToken) {
+        require(_fwbToken != address(0), "MigrationManager: invalid token address");
+        fwbToken = IERC20(_fwbToken);
+    }
+
     /**
-     * 
-     * @param amount amount to transfer 
+     * @notice Deposits tokens to the sender's own account.
+     * @param amount The amount of tokens to deposit.
      */
     function deposit(uint256 amount) external {
-       _deposit(amount, msg.sender);
+        _deposit(msg.sender, amount, msg.sender);
     }
 
-    function depositTo(uint256 amount, address to) external {
-       _deposit(amount, to);
+    /**
+     * @notice Deposits tokens to another specified account.
+     * @param recipient The address of the recipient.
+     * @param amount The amount of tokens to deposit.
+     */
+    function depositTo(address recipient, uint256 amount) public {
+        _deposit(msg.sender, amount, recipient);
     }
 
-    function burn(uint256 amount) public onlyOwner() {
-        migrationToken.burn(amount);
-    } 
-
-    function _deposit(uint256 amount, address to ) private {
-       deposits[msg.sender] += amount;
-       depositsTo[msg.sender][to] += amount;
-       migrationToken.transferFrom(msg.sender, address(this), amount);
-       emit Deposit(msg.sender, to, amount);
+    /**
+     * @notice Retrieves the details of a specific deposit by ID.
+     * @param depositId The ID of the deposit.
+     * @return depositor The address of the depositor.
+     * @return recipient The address of the recipient.
+     * @return amount The amount of tokens deposited.
+     */
+    function getDeposit(uint256 depositId) external view returns (address depositor, address recipient, uint256 amount) {
+        require(depositId < depositCount, "MigrationManager: invalid deposit ID");
+        DepositInfo storage depositInfo = deposits[depositId];
+        return (depositInfo.depositor, depositInfo.recipient, depositInfo.amount);
     }
 
+    function _deposit(address from, uint256 amount, address recipient) internal {
+        require(recipient != address(0), "MigrationManager: invalid recipient address");
+        require(amount > 0, "MigrationManager: amount must be greater than 0");
+        require(fwbToken.transferFrom(from, address(this), amount), "MigrationManager: transfer failed");
+
+        deposits.push(DepositInfo(from, recipient, amount));
+        emit Deposit(from, recipient, amount, depositCount);
+        depositCount++;
+    }
 }
