c4b371c620838e8666fe610dead98437e866b0fd
ysfkel <yusufkel@gmail.com>
2024-06-27 21:44:00 +0400

    floating pragmas

--- src/IFWBToken.sol
+++ src/IFWBToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity 0.8.26;
 
 import {IERC20} from "@openzeppelin/token/ERC20/IERC20.sol";
 
--- test/MigrationDistributor_test.sol
+++ test/MigrationDistributor_test.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity 0.8.26;
 import {Ownable} from "@openzeppelin/access/Ownable.sol";
 import {Test, console} from "forge-std/Test.sol";
 import {MigrationDistributor} from "../src/MigrationDistributor.sol";
